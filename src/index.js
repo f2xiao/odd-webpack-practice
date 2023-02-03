b938f215c1318c3d1aec1716fc80bd75ba80dc70
f2xiao <fxiao.mecheng@gmail.com>
2023-02-03 11:58:22 -0500

    Edit index.js

--- src/index.js
+++ src/index.js
@@ -1,34 +1,38 @@
 // import your function
 import {User, Admin} from './User.js';
 
-
-
-function component() {
+function component(user) {
   const element = document.createElement('div');
+  element.textContent = user.printString();
+  const usersContainer = document.querySelector("#users");
+  usersContainer.appendChild(element);
 
   // use your function!
 
-  const user = User("tom", 26);
-  // console.log(user); 
-  const nameString = user.printString();
+  // console.log(user);
+  // console.log(admin);
+  // console.log(userString);
+  // console.log(adminString);
 
-  console.log(nameString);
+  // console.log(user.__proto__ == admin.__proto__);
+  // console.log(user.__proto__); // proto obj
+  // console.log(user.__proto__.__proto__); // anscestor obj
+  // console.log(user.__proto__.__proto__.__proto__); // Object.prototype or {}
+  // console.log(user.__proto__.__proto__.__proto__.__proto__); // null
 
-  const admin = Admin('pixie', 9);
-
-  console.log(user);
-  console.log(admin);
-  console.log(admin.printString());
-  console.log(user.printString());
-
-  console.log(user.__proto__ == admin.__proto__);
-  console.log(user.__proto__); // proto obj
-  console.log(user.__proto__.__proto__); // anscestor obj
-  console.log(user.__proto__.__proto__.__proto__); // Object.prototype or {}
-  console.log(user.__proto__.__proto__.__proto__.__proto__); // null
-
-  console.log(user.log);
-  console.log(admin.log);
+  // console.log(user.log);
+  // console.log(admin.log);
 }
 
-component();
+function render(moduleName,user, admin) {
+  const element = document.createElement('div');
+  element.id = moduleName;
+  document.body.appendChild(element);
+  component(user);
+  component(admin);
+}
+
+const user = User("tom", 26);
+const admin = Admin('pixie', 9);
+
+render('users',user, admin);
