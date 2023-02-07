153e473df0f790c6789c10f6c3b974577bdf4305
arashi4ever <fxiao.mecheng@gmail.com>
2023-02-07 15:52:26 -0500

    Add devtool for inline source map setting

--- src/print.js
+++ src/print.js
@@ -1,3 +1,2 @@
 export default function printMe() {
-  console.log('I get called from print.js!');
-}
+cosnole.log('I get called from print.js!');}
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@
      index: './src/index.js',
      print: './src/print.js',
    },
+	devtool: 'inline-source-map',
    plugins: [
      new HtmlWebpackPlugin({
            title: 'Development',}),
