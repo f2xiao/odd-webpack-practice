1b119ede39e6ea4a7f9a190cfd7d3e2650bcac7d
arashi4ever <fxiao.mecheng@gmail.com>
2023-02-07 15:30:20 -0500

    End of output-management

--- src/index.js
+++ src/index.js
@@ -1,61 +1,18 @@
-// import your function
-import {User, Admin} from './User.js';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
+import _ from 'lodash';
+import printMe from './print.js';
 
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
+ function component() {
+   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-function component(user) {
-  const element = document.createElement('div');
-  element.textContent = user.printString();
-element.classList.add('hello');
-	  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
+  element.appendChild(btn);
 
-  element.appendChild(myIcon);
+   return element;
+ }
 
-const usersContainer = document.querySelector("#users");
-  usersContainer.appendChild(element);
-
-  // use your function!
-
-  // console.log(user);
-  // console.log(admin);
-  // console.log(userString);
-  // console.log(adminString);
-
-  // console.log(user.__proto__ == admin.__proto__);
-  // console.log(user.__proto__); // proto obj
-  // console.log(user.__proto__.__proto__); // anscestor obj
-  // console.log(user.__proto__.__proto__.__proto__); // Object.prototype or {}
-  // console.log(user.__proto__.__proto__.__proto__.__proto__); // null
-
-  // console.log(user.log);
-  console.log(Data);
-  console.log(Notes);
-  // console.log(admin.log);
-}
-
-function render(moduleName,user) {
-  const element = document.createElement('div');
-  element.id = moduleName;
-  document.body.appendChild(element);
-  component(user);
-}
-
-const user = User("tom", 26);
-
-render('users',user);
+ document.body.appendChild(component());
